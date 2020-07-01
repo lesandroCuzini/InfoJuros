113935ff5246c416c0395356fd6a5f30a1f2b110
lesandroCuzini <lesandro.assis@gmail.com>
2020-07-01 00:19:59 -0300

    feat: rota /taxajuros retornando o valor 1.

---
+++ InfoJuros.Api/Controllers/TaxaJurosController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace InfoJuros.Api.Controllers
+{
+    [ApiController]
+    [Route("/taxajuros")]
+    public class TaxaJurosController : ControllerBase
+    {
+        private readonly ILogger<TaxaJurosController> _logger;
+
+        public TaxaJurosController(ILogger<TaxaJurosController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var percTaxaJuros = 1;
+            return Ok(new { percTaxaJurosMensal = percTaxaJuros });
+        }
+    }
+}
